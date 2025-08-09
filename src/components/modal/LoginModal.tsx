fdda56cc282cf2f13c1899db082b7335c5a76b78
LAPTOP-92NBAG1S\lenovo <nomai6789@gmail.com>
2025-08-10 05:31:24 +0700

    add link for login icon

--- src/components/modal/LoginModal.tsx
+++ src/components/modal/LoginModal.tsx
@@ -142,7 +142,22 @@ const LoginModal: React.FC<LoginModalProps> = ({
                 viewBox='0 0 90 92'
                 fill='none'
               >
-                {/* ...Google icon path */}
+                <path
+                  d='M90 47.1c0-3.1-.3-6.3-.8-9.3H45.9v17.7h24.8c-1 5.7-4.3 10.7-9.2 13.9l14.8 11.5C85 72.8 90 61 90 47.1z'
+                  fill='#4280ef'
+                ></path>
+                <path
+                  d='M45.9 91.9c12.4 0 22.8-4.1 30.4-11.1L61.5 69.4c-4.1 2.8-9.4 4.4-15.6 4.4-12 0-22.1-8.1-25.8-18.9L4.9 66.6c7.8 15.5 23.6 25.3 41 25.3z'
+                  fill='#34a353'
+                ></path>
+                <path
+                  d='M20.1 54.8c-1.9-5.7-1.9-11.9 0-17.6L4.9 25.4c-6.5 13-6.5 28.3 0 41.2l15.2-11.8z'
+                  fill='#f6b704'
+                ></path>
+                <path
+                  d='M45.9 18.3c6.5-.1 12.9 2.4 17.6 6.9L76.6 12C68.3 4.2 57.3 0 45.9.1c-17.4 0-33.2 9.8-41 25.3l15.2 11.8c3.7-10.9 13.8-18.9 25.8-18.9z'
+                  fill='#e54335'
+                ></path>
               </GoogleIcon>
               {t('login.google_login')}
             </GoogleButton>
@@ -153,7 +168,10 @@ const LoginModal: React.FC<LoginModalProps> = ({
                 viewBox='0 0 24 24'
                 fill='none'
               >
-                {/* ...Facebook icon path */}
+                <path
+                  fill='white'
+                  d='M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073c0 6.016 4.388 10.995 10.125 11.854v-8.385H7.078v-3.47h3.047V9.413c0-3.007 1.793-4.668 4.533-4.668 1.312 0 2.686.235 2.686.235v2.953h-1.513c-1.492 0-1.956.927-1.956 1.877v2.26h3.328l-.532 3.47h-2.796v8.385C19.612 23.068 24 18.089 24 12.073z'
+                />
               </FacebookIcon>
               {t('login.facebook_login')}
             </FacebookButton>
@@ -318,12 +336,12 @@ const GoogleButton = styled.button`
 `;
 
 const FacebookButton = styled(GoogleButton)`
-  background: #1877f2;
+  background: #5a81d9;
   color: white;
   border: none;
 
   &:hover {
-    background: #145dbf;
+    background: #486fbb;
   }
 `;
 
