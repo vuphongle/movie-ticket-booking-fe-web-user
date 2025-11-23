6b56f24065a93c83bce5165fe732299e67e8e20d
MaiChienNo <nomai6789@gmail.com>
2025-11-23 15:25:21 +0700

    fix: api

--- src/utils/cancelSeatMultiBeacon.ts
+++ src/utils/cancelSeatMultiBeacon.ts
@@ -1,7 +1,7 @@
 function cancelSeatMultiBeacon(showtimeId: number, seatIds: number[]) {
   if (!seatIds?.length) return;
 
-  const url = `${import.meta.env.VITE_DOMAIN}${import.meta.env.VITE_API_BASE_URL}/seat-reservations/cancel-multiple`;
+  const url = `https://gocinema.io.vn/api/seat-reservations/cancel-multiple`;
 
   const payload = JSON.stringify({ showtimeId, seatIds });
   const blob = new Blob([payload], { type: "application/json" });
